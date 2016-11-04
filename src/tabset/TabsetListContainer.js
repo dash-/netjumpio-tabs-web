01ebde73d07cdf19a89ade89c847d75921f199f2
Basic <basic@localhost>
2016-11-04 16:10:05 -0600

    Adding container for tabset list

---
+++ src/tabset/TabsetListContainer.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import type { Connector } from 'react-redux'
+import TabsetListView from './TabsetListView';
+
+
+function mapStateToProps(state) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+
+const connector = connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
+
+export default connector(TabsetListView);
+
