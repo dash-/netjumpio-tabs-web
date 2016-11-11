ebd443325d973e3ffe9c8e45dc632a9ae007c376
Basic <basic@localhost>
2016-11-11 16:38:18 -0700

    Stub for organization form

---
+++ src/orgs/OrgsForm.js
@@ -0,0 +1,23 @@
+///
+// Dependencies
+///
+
+import React, { Component } from 'react';
+
+
+///
+// View
+///
+
+class OrgsFormView extends Component {
+	render() {
+		return (
+			<div className="orgs-form">
+				Orgs Form
+			</div>
+		);
+	}
+}
+
+export default OrgsFormView;
+
