4ce138cde0c3848ccfef95246905f405f13d331a
Basic <basic@localhost>
2016-11-15 14:26:58 -0700

    Added tabset logo

---
+++ src/elements/TabSetsLogo.js
@@ -0,0 +1,22 @@
+///
+// Dependencies
+///
+
+import React, { Component } from 'react';
+
+
+
+///
+// View
+///
+
+class TabSetsLogoView extends Component {
+	render() {
+		return (
+			<div className="tabsets-logo">TS</div>
+		);
+	}
+}
+
+export default TabSetsLogoView;
+
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,11 +2,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+import TabSetsLogo from '../elements/TabSetsLogo';
+
 class Header extends Component {
   render() {
     return (
     	<div className="app-header-container">
 				<div className="app-header">
+					<TabSetsLogo />
 					<h3 className="title">TabSets</h3>
 					<div className="links">
 						<Link to="/directive">App List</Link>
