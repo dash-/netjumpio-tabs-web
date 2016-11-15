d19f7e9045a4416e629e0c09c500e72ef6aac361
Basic <basic@localhost>
2016-11-15 16:03:22 -0700

    Removing old directive stubs

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,16 +3,17 @@ import {IndexRoute, Route} from 'react-router';
 
 import MainLayout from '../layouts/MainLayout.js';
 import Landing from '../landing/Landing';
-import DirectiveList from '../dirs/DirectiveList';
-import DirectiveForm from '../dirs/DirectiveForm';
 
 const routes = (
 	<Route path="/" component={MainLayout}>
 		<IndexRoute component={Landing} />
+	</Route>
+
+	/*
 		<Route path="directive" component={DirectiveList}>
 			<Route path=":id" component={DirectiveForm} />
 		</Route>
-	</Route>
+	*/
 );
 
 export default routes;
