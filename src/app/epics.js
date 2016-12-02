39cd9c846b004317b960ccbc54b50f174ccfd833
Basic <basic@localhost>
2016-12-02 12:55:42 -0700

    Added user epics and registered with root epic

--- src/app/epics.js
+++ src/app/epics.js
@@ -1,5 +1,6 @@
 
 import { combineEpics } from 'redux-observable';
+import user from '../user/epics';
 import forms from '../forms/epics';
 import groups from '../groups/epics';
 import people from '../people/epics';
@@ -7,6 +8,7 @@ import roles from '../roles/epics';
 import tabsets from '../tabsets/epics';
 
 export default combineEpics(
+	user,
 	forms,
 	groups,
 	people,
---
+++ src/user/epics.js
@@ -0,0 +1,41 @@
+///
+// Dependencies
+///
+
+import { combineEpics } from 'redux-observable';
+import { Observable } from 'rxjs';
+
+import api from '../app/api';
+import * as actions from './actions';
+import * as formsActions from '../forms/actions';
+
+
+///
+// Epics
+///
+
+const login = (action$, store) => (
+	action$.ofType(actions.SUBMIT_LOGIN_FORM)
+		.switchMap(action => (
+			Observable.fromPromise(
+				api.createUserClient('people').login({
+					email: action.payload.email,
+					password: action.payload.password,
+				}, 'user')
+//			).map(payload => formsActions.formSubmitFulfilled('tabsets'))
+			).map(function(payload) {
+console.log('payload', payload);
+				return formsActions.formSubmitFulfilled('tabsets');
+			})
+		))
+);
+
+
+///
+// Export
+///
+
+export default combineEpics(
+	login
+);
+
