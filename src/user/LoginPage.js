e6b81b8c6f8a5874dd5eeb491c9e798c63a793fe
Basic <basic@localhost>
2016-12-02 12:16:01 -0700

    Added button, header, organizational content to login page

--- src/user/LoginPage.js
+++ src/user/LoginPage.js
@@ -4,7 +4,9 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Button from 'react-bootstrap/lib/Button';
 
+import formActions from '../forms/actions';
 import LoginForm from './LoginForm';
 
 
@@ -15,8 +17,15 @@ import LoginForm from './LoginForm';
 class LoginPageView extends Component {
   render() {
     return (
-    	<div className="login-page">
-				<LoginForm />
+    	<div className="page login-page">
+				<div className="login-section col-md-6 col-md-offset-3">
+					<h1>Please sign in</h1>
+					<LoginForm>
+						<Button onClick={this.props.submitForm}>
+							Submit
+						</Button>
+					</LoginForm>
+				</div>
 			</div>
     );
   }
@@ -33,7 +42,9 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return {};
+	return {
+		submitForm: () => dispatch(formActions.submitForm('login')),
+	};
 }
 
 const connector = connect(
