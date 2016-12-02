f11608ba25b88755f357a568beeae424f211d79d
Basic <basic@localhost>
2016-12-02 12:57:25 -0700

    Switched to using onSubmit for form submission, switched to using buildFormSubmitter for handling submit

--- src/user/LoginPage.js
+++ src/user/LoginPage.js
@@ -6,7 +6,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Button from 'react-bootstrap/lib/Button';
 
-import formActions from '../forms/actions';
+import buildFormSubmitter from '../forms/buildFormSubmitter';
 import LoginForm from './LoginForm';
 
 
@@ -20,8 +20,8 @@ class LoginPageView extends Component {
     	<div className="page login-page">
 				<div className="login-section col-md-6 col-md-offset-3">
 					<h1>Please sign in</h1>
-					<LoginForm>
-						<Button onClick={this.props.submitForm}>
+					<LoginForm onSubmit={this.props.submitForm}>
+						<Button type="submit">
 							Submit
 						</Button>
 					</LoginForm>
@@ -36,14 +36,13 @@ class LoginPageView extends Component {
 // Container
 ///
 
-
 function mapStateToProps(state) {
 	return {};
 }
 
 function mapDispatchToProps(dispatch) {
 	return {
-		submitForm: () => dispatch(formActions.submitForm('login')),
+		submitForm: buildFormSubmitter(dispatch, 'login'),
 	};
 }
 
