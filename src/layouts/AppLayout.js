7b8449a51db32eeb51daf7da4ce396395a2928fe
Basic <basic@localhost>
2016-12-02 12:22:51 -0700

    Moving login rendering to ts app layout; non-app pages do not always require login

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -1,26 +1,85 @@
-// Core dependencies
+///
+// Dependencies
+///
+
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Col from 'react-bootstrap/lib/Col';
 
 import Header from './Header';
 import SidePanel from './SidePanel';
+import LoginPage from '../user/LoginPage';
 
-class AppLayout extends Component {
+
+///
+// View
+///
+
+class AppLayoutView extends Component {
+	renderMainPanel() {
+		if(! this.props.isLoggedIn) {
+			return '';
+		}
+
+		return (
+			<Col md={8} className="main-panel">
+				{this.props.children}
+			</Col>
+		);
+	}
+
+	renderSidePanel() {
+		if(! this.props.isLoggedIn) {
+			return '';
+		}
+
+		return (
+			<Col md={4} className="side-panel">
+				<SidePanel />
+			</Col>
+		);
+	}
+
+	renderLoginPage() {
+		if(this.props.isLoggedIn) {
+			return '';
+		}
+
+		return <LoginPage />;
+	}
+
   render() {
     return (
     	<div className="app-layout">
     		<Header />
-    		<Col md={8} className="main-panel">
-					{this.props.children}
-				</Col>
-				<Col md={4} className="side-panel">
-					<SidePanel />
-				</Col>
+				{this.renderLoginPage()}
+				{this.renderMainPanel()}
+				{this.renderSidePanel()}
 			</div>
     );
   }
 }
 
-export default AppLayout;
+
+///
+// Container
+///
+
+function mapStateToProps(state) {
+	return {
+		isLoggedIn: state.getIn(['user', 'isLoggedIn']),
+	};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+const connector = connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
+
+export default connector(AppLayoutView);
 
--- src/layouts/GlobalLayout.js
+++ src/layouts/GlobalLayout.js
@@ -3,9 +3,6 @@
 ///
 
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import LoginPage from '../user/LoginPage';
 
 
 ///
@@ -13,51 +10,13 @@ import LoginPage from '../user/LoginPage';
 ///
 
 class GlobalLayout extends Component {
-	renderChildren() {
-		if(this.props.isLoggedIn) {
-			return this.props.children
-		}
-		return '';
-	}
-
-	renderLoginPage() {
-		if(this.props.isLoggedIn) {
-			return '';
-		}
-
-		return <LoginPage />;
-	}
-
   render() {
     return (
     	<div className="global-layout">
-				{this.renderLoginPage()}
-				{this.renderChildren()}
+				{this.props.children}
 			</div>
     );
   }
 }
 
-
-///
-// Container
-///
-
-
-function mapStateToProps(state) {
-	return {
-		isLoggedIn: state.getIn(['user', 'isLoggedIn']),
-	};
-}
-
-function mapDispatchToProps(dispatch) {
-  return {};
-}
-
-const connector = connect(
-  mapStateToProps,
-  mapDispatchToProps
-);
-
-export default connector(GlobalLayout);
-
+export default GlobalLayout;
