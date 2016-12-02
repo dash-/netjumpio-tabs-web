162862076d52649acee0928867b45ea29e8f0d56
Basic <basic@localhost>
2016-12-02 12:26:24 -0700

    Added user actions w/ submit login form action

---
+++ src/user/actions.js
@@ -0,0 +1,18 @@
+///
+// Dependencies
+///
+
+import * as formsActions from '../forms/actions';
+
+
+///
+// Action types
+///
+
+export const SUBMIT_LOGIN_FORM = formsActions.FORM_SUBMIT_START + ':login';
+
+
+///
+// Action creators
+///
+
