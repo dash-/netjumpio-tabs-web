9c83aa854d9d1802058fc6bd6b0f0a681feaa9e6
Basic <basic@localhost>
2016-12-01 21:33:31 -0700

    Refactored layouts: Broke main layout into global and app layouts

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {IndexRoute, Route} from 'react-router';
 
-import MainLayout from '../layouts/MainLayout.js';
+import GlobalLayout from '../layouts/GlobalLayout.js';
+import AppLayout from '../layouts/AppLayout.js';
 import Landing from '../landing/Landing';
 
 import TabsetsItem from '../tabsets/TabsetsItem';
 
 const routes = (
-	<Route path="/" component={MainLayout}>
+	<Route path="/" component={GlobalLayout}>
 		<IndexRoute component={Landing} />
-		<Route path="tabsets/:id" component={TabsetsItem}>
+		<Route path="app/" component={AppLayout}>
+			<Route path="tabsets/:id" component={TabsetsItem} />
 		</Route>
 	</Route>
 );
