8c2d0273dd90b5d6063d1062e0353d70bf0a32f2
David Ash <basic@localhost>
2016-12-06 22:24:52 -0700

    feat/cr8 - Added aux data form control (hidden input) that adds data to submission actions

---
+++ src/forms/AuxData.js
@@ -0,0 +1,95 @@
+///
+// Dependencies
+///
+
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+
+import * as actions from './actions';
+
+
+///
+// View
+///
+
+class ManagedHiddenFormControlView extends Component {
+	static contextTypes = {
+		formName: PropTypes.string.isRequired,
+	};
+
+
+	///
+	// Hooks
+	///
+
+	componentWillMount() {
+		this.updateValue(this.props.value);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if(this.props.value !== nextProps.value) {
+			this.updateValue(nextProps.value);
+		}
+	}
+
+
+	///
+	// Methods
+	///
+
+	updateValue(value) {
+		const formName = this.context.formName;
+		const fieldName = this.props.name;
+
+		this.props.fieldChanged(formName, fieldName, value);
+	}
+
+	getStateValue() {
+		const keyPath = [
+			this.context.formName, 'auxValues', this.props.name
+		];
+		return this.props.forms.getIn(keyPath, '');
+	}
+
+
+	///
+	// Rendering
+	///
+
+	render() {
+		return (
+			<input
+				type="hidden" 
+				value={this.getStateValue()}
+			/>
+		);
+	}
+}
+
+
+///
+// Container
+///
+
+function mapStateToProps(state) {
+	return {
+		forms: state.get('forms'),
+	};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+		fieldChanged: (form, field, value) => dispatch(
+			actions.auxFieldChanged(form, field, value)
+		),
+  };
+}
+
+const connector = connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
+
+export default connector(ManagedHiddenFormControlView);
+
