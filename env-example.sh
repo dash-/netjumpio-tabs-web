4b66b73ace87343b5c08f9df59316febec692cf0
Jon Barrett <jonab1974@gmail.com>
2016-12-16 23:24:00 +0000

    Updated documentation

--- env-example.sh
+++ env-example.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # This file should be copied outside of this repository, typically to
-# ~/.tabs-app-env.sh, and then customized.  The file can then be imported via
+# ~/.tabs-web-env.sh, and then customized.  The file can then be imported via
 # the `source` command prior to running the web daemon.
 
 export API_BASE_URL="http://localhost:4002/api"
