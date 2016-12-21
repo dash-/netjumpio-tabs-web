c176bad9d33275bb5b7703f65358b4744aae4eae
David Ash <green.neon@gmail.com>
2016-12-20 20:05:30 -0700

    Changed "tab" action value label to "item" for consistency

--- src/tabs/actions.js
+++ src/tabs/actions.js
@@ -10,28 +10,27 @@ import * as formsActions from '../forms/actions';
 ///
 
 export const URL_FORM_SUBMIT = formsActions.FORM_SUBMIT_START + ':tabsUrl';
+export const TABS_FORM_SUBMIT = formsActions.FORM_SUBMIT_START + ':tabs';
 
 export const GET_WEBPAGE_INFO_START = 'TABS:GET_WEBPAGE_INFO_START';
 export const GET_WEBPAGE_INFO_FAIL = 'TABS:GET_WEBPAGE_INFO_FAIL';
 
-export const TABS_FORM_SUBMIT = formsActions.FORM_SUBMIT_START + ':tabs';
+export const ADD_ITEM_START = 'TABS:ADD_ITEM_START';
+export const ADD_ITEM_FAIL = 'TABS:ADD_ITEM_FAIL';
+export const ADD_ITEM_DONE = 'TABS:ADD_ITEM_DONE';
 
-export const ADD_TAB_START = 'TABS:ADD_TAB_START';
-export const ADD_TAB_FAIL = 'TABS:ADD_TAB_FAIL';
-export const ADD_TAB_DONE = 'TABS:ADD_TAB_DONE';
+export const EDIT_ITEM_PROMPT = 'TABS:EDIT_ITEM_PROMPT';
+export const EDIT_ITEM_START = 'TABS:EDIT_ITEM_START';
+export const EDIT_ITEM_FAIL = 'TABS:EDIT_ITEM_FAIL';
+export const EDIT_ITEM_DONE = 'TABS:EDIT_ITEM_DONE';
 
-export const EDIT_TAB_PROMPT = 'TABS:EDIT_TAB_PROMPT';
-export const EDIT_TAB_START = 'TABS:EDIT_TAB_START';
-export const EDIT_TAB_FAIL = 'TABS:EDIT_TAB_FAIL';
-export const EDIT_TAB_DONE = 'TABS:EDIT_TAB_DONE';
+export const REMOVE_ITEM_START = 'TABS:REMOVE_ITEM_START';
+export const REMOVE_ITEM_FAIL = 'TABS:REMOVE_ITEM_FAIL';
+export const REMOVE_ITEM_DONE = 'TABS:REMOVE_ITEM_DONE';
 
-export const REMOVE_TAB_START = 'TABS:REMOVE_TAB_START';
-export const REMOVE_TAB_FAIL = 'TABS:REMOVE_TAB_FAIL';
-export const REMOVE_TAB_DONE = 'TABS:REMOVE_TAB_DONE';
-
-export const RESTORE_TAB_START = 'TABS:RESTORE_TAB_START';
-export const RESTORE_TAB_FAIL = 'TABS:RESTORE_TAB_FAIL';
-export const RESTORE_TAB_DONE = 'TABS:RESTORE_TAB_DONE';
+export const RESTORE_ITEM_START = 'TABS:RESTORE_ITEM_START';
+export const RESTORE_ITEM_FAIL = 'TABS:RESTORE_ITEM_FAIL';
+export const RESTORE_ITEM_DONE = 'TABS:RESTORE_ITEM_DONE';
 
 
 ///
@@ -46,55 +45,55 @@ export function getWebpageInfoFail(err) {
 	return {type: GET_WEBPAGE_INFO_FAIL, payload: err};
 }
 
-export function addTabStart(tabsetId, tab) {
-	return {type: ADD_TAB_START, payload: tab, aux: {tabsetId}};
+export function addItemStart(tabsetId, tab) {
+	return {type: ADD_ITEM_START, payload: tab, aux: {tabsetId}};
 }
 
-export function addTabFail(err) {
-	return {type: ADD_TAB_FAIL, payload: err};
+export function addItemFail(err) {
+	return {type: ADD_ITEM_FAIL, payload: err};
 }
 
-export function addTabDone(tab) {
-	return {type: ADD_TAB_DONE, payload: tab};
+export function addItemDone(tab) {
+	return {type: ADD_ITEM_DONE, payload: tab};
 }
 
-export function editTabPrompt(tab) {
-	return {type: EDIT_TAB_PROMPT, payload: tab};
+export function editItemPrompt(tab) {
+	return {type: EDIT_ITEM_PROMPT, payload: tab};
 }
 
-export function editTabStart(tab) {
-	return {type: EDIT_TAB_START, payload: tab};
+export function editItemStart(tab) {
+	return {type: EDIT_ITEM_START, payload: tab};
 }
 
-export function editTabFail(err) {
-	return {type: EDIT_TAB_FAIL, payload: err};
+export function editItemFail(err) {
+	return {type: EDIT_ITEM_FAIL, payload: err};
 }
 
-export function editTabDone(tab) {
-	return {type: EDIT_TAB_DONE, payload: tab};
+export function editItemDone(tab) {
+	return {type: EDIT_ITEM_DONE, payload: tab};
 }
 
-export function removeTabStart(tab) {
-	return {type: REMOVE_TAB_START, payload: tab};
+export function removeItemStart(tab) {
+	return {type: REMOVE_ITEM_START, payload: tab};
 }
 
-export function removeTabFail(err) {
-	return {type: REMOVE_TAB_FAIL, payload: err};
+export function removeItemFail(err) {
+	return {type: REMOVE_ITEM_FAIL, payload: err};
 }
 
-export function removeTabDone(tab) {
-	return {type: REMOVE_TAB_DONE, payload: tab};
+export function removeItemDone(tab) {
+	return {type: REMOVE_ITEM_DONE, payload: tab};
 }
 
-export function restoreTabStart(tab) {
-	return {type: RESTORE_TAB_START, payload: tab};
+export function restoreItemStart(tab) {
+	return {type: RESTORE_ITEM_START, payload: tab};
 }
 
-export function restoreTabFail(err) {
-	return {type: RESTORE_TAB_FAIL, payload: err};
+export function restoreItemFail(err) {
+	return {type: RESTORE_ITEM_FAIL, payload: err};
 }
 
-export function restoreTabDone(tab) {
-	return {type: RESTORE_TAB_DONE, payload: tab};
+export function restoreItemDone(tab) {
+	return {type: RESTORE_ITEM_DONE, payload: tab};
 }
 
--- src/tabsets/epics.js
+++ src/tabsets/epics.js
@@ -100,19 +100,19 @@ const tabsFormSubmit = (action$, store) => (
 	action$.ofType(tabsActions.TABS_FORM_SUBMIT)
 		.switchMap(action => {
 			if(isUndefined(action.payload.id)) {
-				return Observable.of(tabsActions.addTabStart(
+				return Observable.of(tabsActions.addItemStart(
 					action.aux.tabsetId, action.payload
 				));
 			}
 
-			return Observable.of(tabsActions.editTabStart(
+			return Observable.of(tabsActions.editItemStart(
 				action.payload
 			));
 		})
 );
 
 const addTab = (action$, store) => (
-	action$.ofType(tabsActions.ADD_TAB_START)
+	action$.ofType(tabsActions.ADD_ITEM_START)
 		.switchMap(action => (
 			Observable.fromPromise(
 				api.createRelatedClient({
@@ -124,13 +124,13 @@ const addTab = (action$, store) => (
 			).flatMap(payload => Observable.concat(
 				Observable.of(formsActions.formSubmitDone('tabs')),
 				Observable.of(formsActions.clearFormValues('tabs')),
-				Observable.of(tabsActions.addTabDone(payload))
+				Observable.of(tabsActions.addItemDone(payload))
 			))
 		))
 );
 
 const editTabPrompt = (action$, store) => (
-	action$.ofType(tabsActions.EDIT_TAB_PROMPT)
+	action$.ofType(tabsActions.EDIT_ITEM_PROMPT)
 		.switchMap(action => Observable.concat(
 			Observable.of(formsActions.showForm('tabs')),
 			Observable.of(
@@ -142,7 +142,7 @@ const editTabPrompt = (action$, store) => (
 );
 
 const editTab = (action$, store) => (
-	action$.ofType(tabsActions.EDIT_TAB_START)
+	action$.ofType(tabsActions.EDIT_ITEM_START)
 		.switchMap(action => (
 			Observable.fromPromise(
 				api.createClient('tabs', {
@@ -151,26 +151,26 @@ const editTab = (action$, store) => (
 			).flatMap(payload => Observable.concat(
 				Observable.of(formsActions.formSubmitDone('tabs')),
 				Observable.of(formsActions.clearFormValues('tabs')),
-				Observable.of(tabsActions.editTabDone(payload))
+				Observable.of(tabsActions.editItemDone(payload))
 			))
 		))
 );
 
 const removeTab = (action$, store) => (
-	action$.ofType(tabsActions.REMOVE_TAB_START)
+	action$.ofType(tabsActions.REMOVE_ITEM_START)
 		.switchMap(action => (
 			Observable.fromPromise(
 				api.createClient('tabs', {
 					accessToken: store.getState().getIn(['user', 'accessToken']),
 				}).destroyById(action.payload.id)
 			).flatMap(payload => Observable.of(
-				tabsActions.removeTabDone(action.payload))
+				tabsActions.removeItemDone(action.payload))
 			)
 		))
 );
 
 const restoreTab = (action$, store) => (
-	action$.ofType(tabsActions.RESTORE_TAB_START)
+	action$.ofType(tabsActions.RESTORE_ITEM_START)
 		.switchMap(action => (
 			Observable.fromPromise(
 				api.request('tabs', [
@@ -180,7 +180,7 @@ const restoreTab = (action$, store) => (
 					accessToken: store.getState().getIn(['user', 'accessToken']),
 				})
 			).flatMap(payload => Observable.of(
-				tabsActions.restoreTabDone(payload))
+				tabsActions.restoreItemDone(payload))
 			)
 		))
 );
