47aabbcb6dd41a17110084bf527a41ff52f5d75c
David Ash <green.neon@gmail.com>
2017-01-06 18:35:50 -0700

    feat/cr72 - Generic widget for failure messages with retry button

---
+++ src/elements/FailRetryNotif.js
@@ -0,0 +1,112 @@
+///
+// Dependencies
+///
+
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+import Button from 'react-bootstrap/lib/Button';
+
+import NotificationsListItem from '../elements/NotificationsListItem';
+import NotificationMessage from '../elements/NotificationMessage';
+import NotificationButtons from '../elements/NotificationButtons';
+
+
+///
+// View
+///
+
+class FailRetryNotifView extends Component {
+	///
+	// Construction
+	///
+
+	constructor(props) {
+		super(props);
+
+		this.retryAction = this.retryAction.bind(this);
+		this.renderMessage = this.renderMessage.bind(this);
+	}
+
+
+	///
+	// Event handlers
+	///
+
+	retryAction(action, dismiss) {
+		return () => {
+			this.props.dispatch(action);
+			dismiss();
+		};
+	}
+
+
+	///
+	// Rendering
+	///
+
+	renderMessage(notification, dismiss) {
+		const action = notification.trigger.payload.action;
+		const error = notification.trigger.payload.error;
+		const userMessage = (
+			error.userMessage ? '(' + error.userMessage + ')' : ''
+		);
+
+		const message = this.props.renderMessage(
+			action, userMessage, error
+		);
+
+		return (
+			<NotificationMessage>
+				{message}
+				<NotificationButtons>
+					<Button
+						bsStyle="danger"
+						onClick={this.retryAction(action, dismiss)}
+					>
+						Retry
+					</Button>
+				</NotificationButtons>
+			</NotificationMessage>
+		);
+	}
+
+	render() {
+		return (
+			<NotificationsListItem
+				type="error"
+				triggeredBy={this.props.triggeredBy}
+				renderMessage={this.renderMessage}
+				hideAfter={15000}
+			/>
+		);
+	}
+}
+
+FailRetryNotifView.propTypes = {
+	triggeredBy: PropTypes.string.isRequired,
+	dispatch: PropTypes.func.isRequired,
+};
+
+
+///
+// Container
+///
+
+function mapStateToProps(state) {
+	return {};
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		dispatch,
+	};
+}
+
+const connector = connect(
+	mapStateToProps,
+	mapDispatchToProps
+);
+
+export default connector(FailRetryNotifView);
+
