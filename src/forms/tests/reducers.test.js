a57bee571e209e61d33eeba5461b2af4748123af
David Ash <green.neon@gmail.com>
2017-01-10 18:57:22 -0700

    maint/cr68 - Added forms reducers unit tests

---
+++ src/forms/tests/reducers.test.js
@@ -0,0 +1,168 @@
+import { fromJS } from 'immutable';
+import reducer from '../reducers';
+import * as actions from '../actions';
+
+const defaultForm = fromJS({
+	isVisible: false,
+	allowClear: true,
+	allowHide: true,
+	dataHasInitialized: false,
+	uploads: {},
+	values: {},
+	aux: {},
+});
+
+it('correctly reduces invalid action', () => {
+	const reduction = reducer(
+		fromJS({}), {type: null},
+	).toJS();
+
+	const correctReduction = {};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
+it('correctly reduces INIT_FORM action', () => {
+	const reduction = reducer(
+		fromJS({}), actions.initForm('i', {})
+	).toJS();
+
+	const correctReduction = {
+		i: defaultForm.toJS(),
+	};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
+it('correctly reduces INIT_FORM_DATA action', () => {
+	const reduction = reducer(
+		fromJS({}), actions.initFormData('i', {
+			values: { i: 1, j: 2 },
+			aux: { k: 3, l: 4 },
+		})
+	).toJS();
+
+	const correctReduction = {
+		i: defaultForm.set('values', fromJS({
+			i: 1, j: 2
+		})).set('aux', fromJS({
+			k: 3, l: 4
+		})).set('dataHasInitialized', true).toJS()
+	};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
+
+it('correctly reduces CLEAR_FORM action', () => {
+	const reduction = reducer(fromJS({
+		i: defaultForm.set('values', fromJS({
+			i: 1, j: 2
+		})).set('aux', fromJS({
+			k: 3, l: 4
+		})).toJS()
+	}), actions.clearForm('i')).toJS();
+
+	const correctReduction = {
+		i: defaultForm.toJS(),
+	};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
+it('correctly reduces CLEAR_FORM_VALUES action', () => {
+	const reduction = reducer(fromJS({
+		i: defaultForm.set('values', fromJS({
+			i: 1, j: 2
+		})).toJS()
+	}), actions.clearFormValues('i')).toJS();
+
+	const correctReduction = {
+		i: defaultForm.toJS(),
+	};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
+it('correctly reduces SHOW_FORM action', () => {
+	const reduction = reducer(fromJS({
+		i: defaultForm.toJS(),
+	}), actions.showForm('i')).toJS();
+
+	const correctReduction = {
+		i: defaultForm.set('isVisible', true).toJS()
+	};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
+it('correctly reduces HIDE_FORM action', () => {
+	const reduction = reducer(fromJS({
+		i: defaultForm.set('isVisible', true).toJS()
+	}), actions.hideForm('i')).toJS();
+
+	const correctReduction = {
+		i: defaultForm.toJS()
+	};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
+it('correctly reduces FIELD_CHANGED action', () => {
+	const reduction = reducer(fromJS({
+		i: defaultForm.toJS(),
+	}), actions.fieldChanged('i', 'j', 1)).toJS();
+
+	const correctReduction = {
+		i: defaultForm.set('values', fromJS({
+			j: 1
+		})).toJS()
+	};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
+it('correctly reduces AUX_FIELD_CHANGED action', () => {
+	const reduction = reducer(fromJS({
+		i: defaultForm.toJS(),
+	}), actions.auxFieldChanged('i', 'j', 1)).toJS();
+
+	const correctReduction = {
+		i: defaultForm.set('aux', fromJS({
+			j: 1
+		})).toJS()
+	};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
+it('correctly reduces UPLOAD_IMAGE_START action', () => {
+	const reduction = reducer(fromJS({
+		i: defaultForm.toJS(),
+	}), actions.uploadImageStart('i', 'j', 'k')).toJS();
+
+	const correctReduction = {
+		i: defaultForm.set('uploads', fromJS({
+			j: {isUploading: true}
+		})).toJS()
+	};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
+it('correctly reduces UPLOAD_IMAGE_DONE action', () => {
+	const reduction = reducer(fromJS({
+		i: defaultForm.set('uploads', fromJS({
+			j: {isUploading: true}
+		})).toJS()
+	}), actions.uploadImageDone('i', 'j', 'k')).toJS();
+
+	const correctReduction = {
+		i: defaultForm.set('values', fromJS({
+			j: 'k',
+		})).toJS()
+	};
+
+	expect(reduction).toEqual(correctReduction);
+});
+
